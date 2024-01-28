4a51eeebcfcdd69e7a093a7a8bbcf804e20742c4
John Serrano <andrey_201015@hotmail.com>
2024-01-28 16:29:29 -0500

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": [
+    "./node_modules/dts-cli/conf/eslint-config-react-app/index.js",
+    "./node_modules/eslint-config-prettier/index.js",
+    "plugin:prettier/recommended"
+  ],
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  }
+}
