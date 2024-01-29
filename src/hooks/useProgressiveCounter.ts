9ed53c0a28a8a0b2ac1d7feb93d8cdb1217f7d81
John Serrano <andrey_201015@hotmail.com>
2024-01-29 12:11:43 -0500

    Add component Progressive and hook progressive counter#2

---
+++ src/hooks/useProgressiveCounter.ts
@@ -0,0 +1,56 @@
+// import * as React from 'react';
+import { useState, useCallback, useEffect } from 'react';
+
+export const useProgressiveCounter = (
+  initialValue: number | (() => number),
+  duration = 1500,
+  decimals = 0,
+  delay = 5
+): [number, (value: number | ((prevTarget: number) => number)) => void] => {
+  const [target, setTarget] = useState(initialValue);
+  const [current, setCurrent] = useState(initialValue);
+  const [steps, setSteps] = useState(1);
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const initial =
+    typeof initialValue === 'function' ? initialValue() : initialValue;
+
+  const setValue = useCallback(
+    (value: number | ((prevTarget: number) => number)) => {
+      const nextTarget = typeof value === 'function' ? value(target) : value;
+      const steps = Math.max(Math.floor(duration / delay), 1);
+
+      setSteps(steps);
+      setTarget(nextTarget);
+      setCurrentStep(1);
+      setCurrent(lerp(initial, nextTarget, easeOutCubic(1 / steps)));
+    },
+    [delay, duration, target, initial]
+  );
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const progress = currentStep / steps;
+      if (progress === 1) {
+        setCurrent(target);
+      } else {
+        setCurrent(lerp(initial, target, easeOutCubic(progress)));
+        setCurrentStep(currentStep + 1);
+      }
+    }, delay);
+
+    return () => clearTimeout(timeout);
+  }, [delay, currentStep, target, initial, steps]);
+
+  const value = Number(current.toFixed(decimals));
+
+  return [value, setValue];
+};
+
+const lerp = (a: number, b: number, alpha: number): number => {
+  return a + alpha * (b - a);
+};
+
+const easeOutCubic = (value: number): number => {
+  return 1 - Math.pow(1 - value, 3);
+};
